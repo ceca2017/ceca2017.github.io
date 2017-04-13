cc274f6d8776e3b277d5edde7581d837ef5ce87d
claes <claesca@outlook.com>
2017-04-13 15:30:16 +0200

    Modified index to allow conversion in person.js instead of in index

--- src/person.js
+++ src/person.js
@@ -1,13 +1,13 @@
 function Person(attr) {
     var w;
     var h;
-    if (attr[measure_type] == 'metric') {
-      w = attr[weight];
-      h = attr[height];
+    if (attr['measure_type'] == 'metric') {
+      w = attr['weight'];
+      h = attr['height'];
     }
-    else if (attr[measure_type] == 'imperial') {
-      w = attr[weight_lb]/2.2046;
-      h = (attr[height_ft]*12 + attr[height_in])*2.54;
+    else if (attr['measure_type'] == 'imperial') {
+      w = attr['weight_lb']/2.2046;
+      h = (attr['height_ft']*12 + attr['height_in'])*2.54;
     }
     this.weight = w;
     this.height = h;
